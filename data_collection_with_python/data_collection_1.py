15c03f55bc785a8d9dc8f9388071d051d21f2014
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2021-10-31 18:07:37 -0700

    master of the machine

---
+++ data_collection_with_python/data_collection_1.py
@@ -0,0 +1,69 @@
+from serial import *
+from time import localtime,strftime,sleep
+
+if __name__ == "__main__":
+    #figure out what port is really used
+    iStr = "What is the number of the port being used? It is the com port selected in the arduino IDE. Enter the number only: "
+    arduino_port = f"COM{input(iStr)}"
+    baud = 9600 #arduino uno runs at 9600 baud
+    current_time = localtime()
+    fTime= strftime('y%Y_m%m_d%d_h%H_m%M', current_time)
+    fileName = f"collected_data/color_data_{fTime}.csv" #name of csv generated
+    header = "input,colorTemp,lux,R,G,B"
+    try:
+        ser = Serial(
+            port=arduino_port,
+            baudrate=9600,
+            timeout=2,
+            xonxoff=0,
+            rtscts=0,
+            interCharTimeout=None
+        )
+    except (SerialException,FileNotFoundError):
+        print("Aborting program... wrong port / port in use / nothin in port")
+        sleep(4)
+        sys.exit()
+
+    print(f"Connected to Arduino port: {arduino_port}")
+    print("Creating .csv file...")
+    try:
+        file = open(fileName, "w")
+        file.write("")
+        file.write(f"{header}\n")
+    except:
+        print("Couldn't write file and idk how exceptions work with that so you're SOL maybe don't have bad file names or something")
+        if(file.closed() != True):
+            file.close()
+
+    print("File created!")
+    i = "e"
+    print("Ready to collect data! Please note that data collection is sometimes weird but that's just comm error. Simply record another data point. \nType end to end, otherwise input test name:")
+    while True:
+        i = input("Input: ")
+        ser.flushInput()
+        sleep(0.25)
+        if(i == "end"):
+            print("Ending collection...")
+            #close the file
+            file.close()
+            break
+        
+        try:
+            #get and display data to terminal
+            getData = str(ser.readline())
+            print(getData)
+            data = getData[0:][2:-5]
+            file.write(str(i) + "," + data +"\n")
+        except IOError:
+            print("Error writing data")
+        except TypeError(str):
+            print("invalid str in")
+        except SerialException:
+            print("Problem collecting data from Arduino")
+        except SerialTimeoutException:
+            print("Connection timed out")
+
+        
+    if(file.closed != True):
+        #close the file if not already closed
+        file.close()
