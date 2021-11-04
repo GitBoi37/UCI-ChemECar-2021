86e44831206e966b77baf559efd9bd27121d8815
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2021-11-04 13:33:34 -0700

    collected a shitton of data

--- data_collection_with_python/data_collection_1.py
+++ data_collection_with_python/data_collection_1.py
@@ -36,10 +36,10 @@ def getPort():
 
 
 
-def makeFileTimeBased(header, location):
+def makeFileTimeBased(header, location, name):
     current_time = localtime()
     fTime= strftime('y%Y_m%m_d%d_h%H_m%M', current_time)
-    fileName = f"{location}/time_based_data{fTime}.csv" #name of csv generated
+    fileName = f"{location}/{name}_time_based_data_{fTime}.csv" #name of csv generated
     print("Checking directory...")
     try:
         os.makedirs(location)
@@ -48,7 +48,6 @@ def makeFileTimeBased(header, location):
         # directory already exists
         print(e)
         pass
-    header = "input,colorTemp,lux,R,G,B"
 
     print("Creating .csv file...")
     try:
@@ -65,10 +64,10 @@ def makeFileTimeBased(header, location):
 
 
 
-def makeFileEventBased(header, location):
+def makeFileEventBased(header, location, name):
     current_time = localtime()
     fTime= strftime('y%Y_m%m_d%d_h%H_m%M', current_time)
-    fileName = f"{location}/event_based_data_{fTime}.csv" #name of csv generated
+    fileName = f"{location}/{name}_event_based_data_{fTime}.csv" #name of csv generated
     print("Checking directory...")
     try:
         os.makedirs(location)
@@ -107,34 +106,31 @@ def collectTimeBasedData(ser, file):
                 print("Error in input, try again")
         
         print("Data collection time!")
-        cum_time = 0
+        abs_start = time.time()
         while True:  
-            s_time = time.time()
-            print("s_time" + str(s_time))
+            loopStart = time.time()
             ser.flushInput()
-            sleep(0.1)
             try:
                 while(True):
                     #get and display data to terminal
                     ser.flushInput()
-                    sleep(0.25)
                     getData = str(ser.readline())
-                    len2 = len(getData[0:].split(","))
-                    len3 = len(getData.split(","))
-                    print(f"Raw data, len 1, len2, len 3: {getData} , {len(getData[0:])} , {len2} , {len3}")
+                    dataLen= len(getData[0:].split(","))
+                    #len3 = len(getData.split(","))
+                    #print(f"Raw data, len 1, len2, len 3: {getData} , {len(getData[0:])} , {len2} , {len3}")
                     data = getData[0:][2:-5]
-                    if(len2 < 6):
+                    print(f"Data: {data}")
+                    if(dataLen < 6):
                         print("invalid data point, remeasuring...")
                     else:
                         break
-                delta = time.time() - s_time
-                print("delta" + str(delta))
+                delta = time.time() - loopStart
                 if(delta < deltaT):
                     sleep(deltaT - delta)
-                print("cum_time" + str(cum_time))
-                cum_time += deltaT
-                file.write(f"{cum_time},{data}\n")
-                if(cum_time > maxT):
+
+                timeSinceStart = time.time() - abs_start
+                file.write(f"{timeSinceStart},{data}\n")
+                if(timeSinceStart > maxT):
                     break
 
                 
@@ -187,13 +183,28 @@ def collectEventBasedData(ser, file):
         except SerialTimeoutException:
             print("Connection timed out")
 
-
+def getFile():
+    file = None
+    mode = ""
+    while(True):
+        mode = input("Time or event based data collection? Enter T or E: ")
+        name = input("Enter test name to append to file name: ")
+        if(mode == "T"):
+            header = "time(s),colorTemp,lux,R,G,B"
+            file = makeFileTimeBased(header, location, name)
+            break
+        elif(mode == "E"):
+            header = "input,colorTemp,lux,R,G,B"
+            file = makeFileEventBased(header, location, name)
+            break
+        else:
+            print("Invalid input")
+    return (file,mode)
 
 if __name__ == "__main__":
     baud = 9600 #arduino uno runs at 9600 baud
     tout = 2 #timeout for serial input
     location = "data"
-    header = "input,colorTemp,lux,R,G,B"
     file = None
     mode = ""
     print("Starting up...")
@@ -203,30 +214,7 @@ if __name__ == "__main__":
     print("Connecting to serial port...")
 
     ser = connect(arduino_port, tout, baud)
-
-    '''
-    try:
-        current_time = localtime()
-        fTime= strftime('y%Y_m%m_d%d_h%H_m%M', current_time)
-        fileName = f"{location}/event_based_data_{fTime}.csv"
-        file = open(fileName, "w")
-        file.write("")
-        file.write(f"{header}\n")
-    except Exception as e:
-        print(e)
-    '''
-
-    while(True):
-        mode = input("Time or event based data collection? Enter T or E: ")
-        if(mode == "T"):
-            file = makeFileTimeBased(header, location)
-            break
-        elif(mode == "E"):
-            file = makeFileEventBased(header, location)
-            break
-        else:
-            print("Invalid input")
-    
+    (file, mode) = getFile()
 
     if(mode == "T"):
         print("Beginnning time based data collection")
