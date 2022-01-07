010f0e7794fdf9e003621a6f2e78c5419774543f
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2022-01-06 18:07:39 -0800

    made .bat scripts to automatically create executables and delete irrelevant files

---
+++ data_collection_with_python/build_data_collection.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %localhost%
+Title Building .exe...  
+pyinstaller --onefile --console ".\data_collection.py"
+copy .\dist\data_collection.exe
+@RD /S /Q build , __pycache__ , dist
+del .\data_collection.spec
---
+++ data_collection_with_python/build_data_collection_manual_input.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %localhost%
+Title Building .exe...  
+pyinstaller --onefile --console ".\data_collection_manual_input.py"
+copy .\dist\data_collection_manual_input.exe
+@RD /S /Q build , __pycache__ , dist
+del .\data_collection_manual_input.spec
