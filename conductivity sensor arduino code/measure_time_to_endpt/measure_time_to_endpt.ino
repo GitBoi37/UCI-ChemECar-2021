64eb32522ec5f2babde25e13caef0f84e0c8b8d4
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2022-03-11 09:46:33 -0800

    updated calibration and endpt code

--- conductivity sensor arduino code/measure_time_to_endpt/measure_time_to_endpt.ino
+++ conductivity sensor arduino code/measure_time_to_endpt/measure_time_to_endpt.ino
@@ -1,34 +1,20 @@
 #include <DFRobot_EC.h>
-
-
-/*
- * file DFRobot_EC.ino
- * @ https://github.com/DFRobot/DFRobot_EC
- *
- * This is the sample code for Gravity: Analog Electrical Conductivity Sensor / Meter Kit V2 (K=1.0), SKU: DFR0300.
- * In order to guarantee precision, a temperature sensor such as DS18B20 is needed, to execute automatic temperature compensation.
- * You can send commands in the serial monitor to execute the calibration.
- * Serial Commands:
- *   enterec -> enter the calibration mode
- *   calec -> calibrate with the standard buffer solution, two buffer solutions(1413us/cm and 12.88ms/cm) will be automaticlly recognized
- *   exitec -> save the calibrated parameters and exit from calibration mode
- *
- * Copyright   [DFRobot](http://www.dfrobot.com), 2018
- * Copyright   GNU Lesser General Public License
- *
- * version  V1.0
- * date  2018-03-21
- */
-
-
 #include "DFRobot_EC.h"
 #include <EEPROM.h>
-
 #define EC_PIN A1
-float voltage,ecValue,temperature = 25;
-DFRobot_EC ec;
+
+/* Hello proud data collector!
+ * This code is to be used to collect conductivity data with a static endpoint between trials
+ * It'll stop once it reaches the endpoint hardcoded right below this but only if within +- range of it
+ * It spits out a time anyways so if it skips over it just manually take the time and restart it or something
+ * 
+ */
 float endpoint = 10.5;
-int x = 0;
+
+float range = 0.05;
+
+float voltage,ecValue,temperature = 25; //don't need to tweak these pretty sure
+DFRobot_EC ec;
 unsigned long startTime;
 unsigned long thousand = 1000;
 void setup()
@@ -63,7 +49,7 @@ void loop()
         Serial.print("ms/cm Time: ");
         Serial.println(float((millis() - startTime))/float(thousand));
         //if value is within 0.05 range of endpoint then stop the loop
-        if(abs(endpoint - ecValue) < 0.05){
+        if(abs(endpoint - ecValue) < range){
           Serial.print("Time to endpoint: ");
           Serial.println(float(millis() - startTime)/float(thousand));
           break;
