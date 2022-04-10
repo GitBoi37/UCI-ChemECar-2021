030471e98a6b1b25e30f36f043dcfb6d112ef4ed
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2022-04-10 00:27:27 -0700

    fixed one thing and got final car control
    might wanna add a 5 second delay to make sure everything is chilling but otherwise it works and I tested it with both protoboards and it works

--- car control/final_car_control/final_car_control.ino
+++ car control/final_car_control/final_car_control.ino
@@ -11,9 +11,15 @@ float targetValue = 3;
 
 //for button
 const int button_pin = 2;
+
+//for relay
+int Relay = 3;
+
+
 //runs once when the arduino turns on
 void setup() {
   pinMode(button_pin, INPUT);
+  pinMode(Relay, OUTPUT);
   Serial.begin(115200); 
 }
 
@@ -23,18 +29,24 @@ void loop() {
   pinValue = digitalRead(button_pin);
   //read pinValue to determine if button has been pressed
   if(pinValue == HIGH){
-    
     //once the button has been pressed do loop code until some condition is met
-    //normally you would read the sensor value here in place of numloops
+    delay(250);
+    Serial.print("Button pushed, checking conductivity: ");
     while(abs(targetValue - ecValue) > endrange){
+      digitalWrite(Relay, HIGH);
       voltage = analogRead(EC_PIN)/1024.0*5000;
       ecValue = ec.readEC(voltage,temperature);
-      //also normally this would just be something like powermotor() or digitalpin(4, HIGH) to open a relay to power the motor
-      Serial.print("Button pushed, checking conductivity: ");
       Serial.print(ecValue);
       Serial.println("ms/cm");
       delay(100);
+      pinValue = digitalRead(button_pin);
+      if(pinValue == HIGH){
+        Serial.println("Eject button pressed");
+        break;
+      }
     }
-    Serial.println("ENDPOINT REACHED! Resetting");
+    Serial.println("ENDPOINT REACHED! Resetting...");
+    digitalWrite(Relay, LOW);
+    delay(250);
   }
 }
